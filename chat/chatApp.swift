f7f18eceb4afb72829e4a8f11778df0ac685c4e8
Clement VABRE <clemvabre74@gmail.com>
2025-01-08 18:53:51 +0100

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Clement on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
